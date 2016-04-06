b7610b7c202389b24ebc4ac6569b89458e96a6ca
Nhomar Hernández <nhomar@vauxoo.com>
2016-04-06 01:38:01 -0500

    [FIX]. poc - rebase

--- vxscreenshots/watch.py
+++ vxscreenshots/watch.py
@@ -20,13 +20,6 @@ ch.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
-# 'application' code
-logger.debug('debug message')
-logger.info('info message')
-logger.warn('warn message')
-logger.error('error message')
-logger.critical('critical message')
-click.echo('Fuck')
 
 class S3Element(LoggingEventHandler):
 
