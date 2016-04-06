98f8f12d9f9adfa621f582cebfe90dd13d377f90
Nhomar Hernández <nhomar@vauxoo.com>
2016-04-06 00:36:03 -0500

    [IMP] cli converted to click.

--- vxscreenshots/watch.py
+++ vxscreenshots/watch.py
@@ -1,18 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import sys
 import os
 from os import makedirs
 from os.path import isdir, dirname
 import time
 import logging
 import boto3
+import click
 import sqlite3
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from .config import read_config
 config = read_config()
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
 
 
 class S3Element(LoggingEventHandler):
@@ -26,9 +30,9 @@ class S3Element(LoggingEventHandler):
         self.db = config.get('vxscreenshots.database')
         if not isdir(dirname(self.db)):
             makedirs(dirname(self.db))
-        logging.info('Cache dbname: %s' % self.db)
-        logging.info('Bucket name: %s' % self.bucket)
-        logging.info('Folder name: %s' % self.folder)
+        logger.info('Cache dbname: %s' % self.db)
+        logger.info('Bucket name: %s' % self.bucket)
+        logger.info('Folder name: %s' % self.folder)
 
     def get_conn(self):
         return self.conn.cursor()
@@ -55,7 +59,7 @@ class S3Element(LoggingEventHandler):
         self.send_to_s3(what, event)
 
     def send_to_s3(self, what, event):
-        logging.info("Screenshot was Modified %s: %s", what, event.src_path)
+        logger.info("Screenshot was Modified %s: %s", what, event.src_path)
         name, ext = os.path.splitext(event.src_path)
         if what != 'directory' and \
            os.path.isfile(event.src_path) and \
@@ -67,50 +71,43 @@ class S3Element(LoggingEventHandler):
             bucket.put_object(Key=fname, Body=data, ACL='public-read',
                               ContentType='image/%s' % ext[1:])
             self.url = self.get_url(fname)
-            logging.info("Screenshot was pushed to %s" % self.url)
+            logger.info("Screenshot was pushed to %s" % self.url)
             self.conn = sqlite3.connect(self.db)
             # Asume if running as a main script it is a dev mode
             self.cursor = self.get_conn()
             try:
                 self.init_db()
             except Exception, e:
-                logging.warning(e)
+                logger.warning(e)
             try:
                 self.db_insert_new([(event.src_path, self.url, True)])
-                logging.warning('Inserted on db %s %s' % (event.src_path,
-                                                          self.url))
+                logger.warning('Inserted on db %s %s' % (event.src_path,
+                                                         self.url))
             except Exception, e:
-                logging.warning('I could not insert on cache %s' % e)
+                logger.warning('I could not insert on cache %s' % e)
 
 
-def cli():
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    # TODO: convert to click commands
-    if '--help' in sys.argv:
-        print '''usage: command [path to watch] [amazon bucket] [folder in bucket]
-If no arg provided it will read values on user config file
-on ~/.vxscreenshots/vxscreenshots.ini'''
-        exit()
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    bucket = sys.argv[2] if len(sys.argv) > 2 else ''
-    folder = sys.argv[3] if len(sys.argv) > 3 else ''
-    event_handler = S3Element(bucket or config.get('vxscreenshots.bucket_name'),
-                              folder or config.get('vxscreenshots.folder'))
+@click.option('--path', default=config.get('vxscreenshots.supervised'),
+              help='Path to be supervised')
+@click.option('--bucket', default=config.get('vxscreenshots.bucket_name'),
+              help='Bucket where things will be saved to')
+@click.option('--folder', default=config.get('vxscreenshots.folder'),
+              help='Inside the bucket how the folder will be named')
+@click.command()
+def cli(path, bucket, folder):
+    '''Watch a folder and push images automatically to amazon S3'''
+    event_handler = S3Element(bucket, folder),
+    msg = 'Sending to this bucket %s %s %s' % (bucket, folder, path)
+    click.echo(msg)
+    logger.info(msg)
     observer = Observer()
-    if path == '.':
-        path = config.get('vxscreenshots.supervised')
-    logging.info('Watching this Folder: %s' % path)
-    if not isdir(path):
-        makedirs(path)
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
     try:
         while True:
             time.sleep(1)
     except IOError:
-        logging.info('A crazy file changed')
+        logger.info('A crazy file changed')
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
