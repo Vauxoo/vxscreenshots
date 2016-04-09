c2fb9cb24a8898d474d1da12dfb1738b66e2f330
Nhomar Hernández <nhomar@vauxoo.com>
2016-04-09 00:15:17 -0500

    Bump version: 2.4.2 → 2.4.3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except Exception, e:
 
 setup(
     name='vxscreenshots',
-    version='2.4.2',
+    version='2.4.3',
     description="Basic Screenshots manager pushing and sharing automatically "
                 "to Amazon S3",
     long_description=readme + '\n\n' + history,
